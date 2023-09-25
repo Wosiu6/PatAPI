a1dcc470224ef1dbee649fb661664572cf2def92
Pat <wos.patryk3@gmail.com>
2023-09-25 13:51:25 +0100

    website enhanctments

--- Infrastructure/Models/Physics/Canvas.cs
+++ Infrastructure/Models/Physics/Canvas.cs
@@ -8,43 +8,28 @@ namespace Infrastructure.Models.Physics
         public double Width { get; private set; }
         public double Height { get; private set; }
 
+        public Canvas()
+        {
+            Random rand = new();
+
+            double xVel = RandomVelocity(rand, BallMovementConstants.MinimumSpeed, BallMovementConstants.MaximumSpeed);
+            double yVel = RandomVelocity(rand, BallMovementConstants.MinimumSpeed, BallMovementConstants.MaximumSpeed);
+
+            Balls.Add(new Ball(
+                1,
+                Height,
+                new ForceVector(xVel, yVel),
+                radius: rand.NextInt64(5, 15),
+                color: DrawingUtils.GenerateRandomColor().ToString()
+            ));
+        }
+
         public void Resize(double width, double height) =>
             (Width, Height) = (width, height);
 
-        public void StepForward()
-        {
-            foreach (Ball ball in Balls)
-                ball.Move(Width, Height);
-        }
-
         private double RandomVelocity(Random rand, double min, double max)
         {
-            double v = min + (max - min) * rand.NextDouble();
-            if (rand.NextDouble() > .5)
-                v *= -1;
-            return v;
-        }
-
-        public void AddRandomBalls(int count = 10)
-        {
-            double minSpeed = .5;
-            double maxSpeed = 5;
-            double radius = 10;
-            Random rand = new Random();
-
-            for (int i = 0; i < count; i++)
-            {
-                Balls.Add(
-                    new Ball(
-                        x: Width * rand.NextDouble(),
-                        y: Height * rand.NextDouble(),
-                        xVel: RandomVelocity(rand, minSpeed, maxSpeed),
-                        yVel: RandomVelocity(rand, minSpeed, maxSpeed),
-                        radius: radius,
-                        color: DrawingUtils.GenerateRandomColor().ToString()
-                    )
-                );
-            }
+            return min + (max - min) * rand.NextDouble();
         }
     }
 }
---
+++ Infrastructure/Models/Physics/ForceVector.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace Infrastructure.Models.Physics
+{
+    public class ForceVector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public ForceVector(double x, double y) => (X, Y) = (x, y);
+    }
+}
