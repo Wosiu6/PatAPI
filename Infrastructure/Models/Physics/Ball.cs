030fd50c22e0353c68affbda40ba20d73e304359
Pat <wos.patryk3@gmail.com>
2023-09-27 17:30:11 +0100

    balls are jumping

--- Infrastructure/Models/Physics/Ball.cs
+++ Infrastructure/Models/Physics/Ball.cs
@@ -11,21 +11,27 @@ namespace Infrastructure.Models.Physics
         public string Color { get; private set; }
 
         public Ball(double x, double y, ForceVector force, double radius, string color)
-            => (X, Y, Force, Radius, Color) = (x, y, force, radius, color);
+        {
+            Radius = radius;
+            X = Radius;
+            Y = Radius;
+            Force = force;
+            Color = color;
+        }
 
         public void Move60Fps(double width, double height)
         {
             Force.Add(BallMovementConstants.Gravity);
 
-            X += Force.X;
-            Y += Force.Y;
+            X += Force.X * BallMovementConstants.TimeStep;
+            Y += Force.Y * BallMovementConstants.TimeStep;
 
-            if (X < 0.0 || X > width)
+            if (X < 0.0 + Radius || X > width - Radius)
             {
                 Force.X = -Force.X;
                 X = Math.Max(0.0, Math.Min(X, width));
             }
-            if (Y < 0.0 || Y > height)
+            if (Y < 0.0 + Radius || Y > height - Radius*1.1)
             {
                 Force.Y = -Force.Y;
                 Y = Math.Max(0.0, Math.Min(Y, height));
